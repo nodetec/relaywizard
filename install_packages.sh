ccf671b2355ccfca2ef5617efefd7b61bf65652d
Christian Chiarulli <chris.machine@pm.me>
2024-06-19 04:26:05 -0400

    improve install script

--- install_packages.sh
+++ install_packages.sh
@@ -5,41 +5,46 @@ command_exists() {
   command -v "$1" >/dev/null 2>&1
 }
 
+# Function to print messages in green with a checkmark
+print_green() {
+  echo -e "\033[0;32m✓ $1\033[0m"
+}
+
 echo "Updating package lists..."
-apt update
+apt update -qq >/dev/null 2>&1
 
 # Check if nginx is installed, install if not
 if command_exists nginx; then
-  echo "nginx is already installed."
+  print_green "nginx is already installed."
 else
   echo "Installing nginx..."
-  apt install -y nginx
+  apt install -y -qq nginx >/dev/null 2>&1
 fi
 
 # Check if Certbot is installed, install if not
 if command_exists certbot; then
-  echo "Certbot is already installed."
+  print_green "Certbot is already installed."
 else
   echo "Installing Certbot and dependencies..."
-  apt install -y certbot python3-certbot-nginx
+  apt install -y -qq certbot python3-certbot-nginx >/dev/null 2>&1
 fi
 
 # Check if the nostr relay dependencies are installed, install if not
 if dpkg -l | grep -qw build-essential && dpkg -l | grep -qw cmake && dpkg -l | grep -qw protobuf-compiler && dpkg -l | grep -qw pkg-config && dpkg -l | grep -qw libssl-dev && command_exists git; then
-  echo "All nostr relay dependencies are already installed."
+  print_green "All nostr relay dependencies are already installed."
 else
   echo "Installing dependencies for nostr relay..."
-  apt install -y build-essential cmake protobuf-compiler pkg-config libssl-dev git
+  apt install -y -qq build-essential cmake protobuf-compiler pkg-config libssl-dev git >/dev/null 2>&1
 fi
 
 # Check if Rust is installed, install if not
 if command_exists rustc; then
-  echo "Rust is already installed."
+  print_green "Rust is already installed."
 else
   echo "Installing Rust..."
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
   echo "Sourcing Rust environment..."
-  source "$HOME/.cargo/env"
+  source $HOME/.cargo/env
 fi
 
