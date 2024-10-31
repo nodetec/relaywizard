3191e6bb07ea374f5c67a36915a590ff4ee114e2
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2024-10-30 21:24:13 -0400

    chore: update firewall to allow ssh, disable logging, and enable firewall

--- pkg/network/firewall.go
+++ pkg/network/firewall.go
@@ -10,21 +10,35 @@ import (
 // Function to configure the firewall
 func ConfigureFirewall() {
 	spinner, _ := pterm.DefaultSpinner.Start("Configuring firewall...")
-	exec.Command("ufw", "enable").Run()
+	// Allow SSH connections
+	err := exec.Command("ufw", "allow", "ssh").Run()
+	if err != nil {
+		pterm.Println()
+		pterm.Error.Println(fmt.Sprintf("Failed to allow SSH: %v", err))
+		os.Exit(1)
+	}
 
 	// Allow HTTP and HTTPS traffic
-	err := exec.Command("ufw", "allow", "Nginx Full").Run()
+	err = exec.Command("ufw", "allow", "Nginx Full").Run()
 	if err != nil {
 		pterm.Println()
 		pterm.Error.Println(fmt.Sprintf("Failed to allow Nginx Full: %v", err))
 		os.Exit(1)
 	}
 
-	// Reload the firewall to apply the changes
-	err = exec.Command("ufw", "reload").Run()
+	// Disable logging
+	err = exec.Command("ufw", "logging", "off").Run()
 	if err != nil {
 		pterm.Println()
-		pterm.Error.Println(fmt.Sprintf("Failed to reload firewall: %v", err))
+		pterm.Error.Println(fmt.Sprintf("Failed to disable logging: %v", err))
+		os.Exit(1)
+	}
+
+	// Enable the firewall to apply the changes
+	err = exec.Command("ufw", "--force", "enable").Run()
+	if err != nil {
+		pterm.Println()
+		pterm.Error.Println(fmt.Sprintf("Failed to enable firewall: %v", err))
 		os.Exit(1)
 	}
 
