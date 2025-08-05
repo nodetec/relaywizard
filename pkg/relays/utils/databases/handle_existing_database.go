3d6fef453c7a5e4fb463669cc1781f416ce2f7d5
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2025-08-05 14:51:38 -0400

    feat: display the database action selector again if the user selects no when asked to overwrite the db

--- pkg/relays/utils/databases/handle_existing_database.go
+++ pkg/relays/utils/databases/handle_existing_database.go
@@ -6,6 +6,80 @@ import (
 	"os"
 )
 
+func selectDatabaseActionOption(databaseBackupsDirPath, databaseFilePath, backupFileNameBase, relayName string) string {
+	ThemeDefault := pterm.ThemeDefault
+
+	// Supported database action options
+	options := []string{BackupDatabaseFileOption, UseExistingDatabaseFileOption, OverwriteDatabaseFileOption}
+
+	databaseActionSelector := pterm.InteractiveSelectPrinter{
+		TextStyle:     &ThemeDefault.PrimaryStyle,
+		DefaultText:   "Please select an option",
+		Options:       []string{},
+		OptionStyle:   &ThemeDefault.DefaultText,
+		DefaultOption: "",
+		MaxHeight:     3,
+		Selector:      ">",
+		SelectorStyle: &ThemeDefault.SecondaryStyle,
+		Filter:        true,
+	}
+
+	pterm.Println()
+	selectedDatabaseActionOption, _ := databaseActionSelector.WithOptions(options).Show()
+
+	// Display the selected option to the user with a green color for emphasis
+	pterm.Info.Printfln("Selected option: %s", pterm.Green(selectedDatabaseActionOption))
+
+	pterm.Println()
+	var howToHandleExistingDatabase string
+
+	if selectedDatabaseActionOption == BackupDatabaseFileOption {
+		pterm.Println(pterm.LightCyan("Creating database backup..."))
+		pterm.Println()
+		BackupDatabase(databaseBackupsDirPath, databaseFilePath, backupFileNameBase, relayName)
+		howToHandleExistingDatabase = BackupDatabaseFileOption
+	} else if selectedDatabaseActionOption == UseExistingDatabaseFileOption {
+		pterm.Println(pterm.LightCyan("Using existing database..."))
+		pterm.Println()
+		howToHandleExistingDatabase = UseExistingDatabaseFileOption
+	} else if selectedDatabaseActionOption == OverwriteDatabaseFileOption {
+		prompt := pterm.InteractiveContinuePrinter{
+			DefaultValueIndex: 0,
+			DefaultText:       "Overwrite database?",
+			TextStyle:         &ThemeDefault.PrimaryStyle,
+			Options:           []string{"no", "yes"},
+			OptionsStyle:      &ThemeDefault.SuccessMessageStyle,
+			SuffixStyle:       &ThemeDefault.SecondaryStyle,
+			Delimiter:         ": ",
+		}
+
+		pterm.Println(pterm.Yellow("Warning: Are you sure you want to overwrite your existing database?"))
+		pterm.Printfln(pterm.Yellow("If you select 'yes', then the following database will be overwritten: %s"), databaseFilePath)
+		pterm.Println()
+
+		result, _ := prompt.Show()
+
+		if result == "no" {
+			howToHandleExistingDatabase = result
+		} else if result == "yes" {
+			pterm.Println()
+			pterm.Println(pterm.LightCyan("Database will be overwitten..."))
+			pterm.Println()
+			howToHandleExistingDatabase = OverwriteDatabaseFileOption
+		} else {
+			pterm.Println()
+			pterm.Error.Println("Failed to confirm database overwrite action")
+			os.Exit(1)
+		}
+	} else {
+		pterm.Println()
+		pterm.Error.Println(("Failed to perform selected database action"))
+		os.Exit(1)
+	}
+
+	return howToHandleExistingDatabase
+}
+
 // Function to handle existing database during install
 func HandleExistingDatabase(databaseBackupsDirPath, databaseFilePath, backupFileNameBase, relayName string) string {
 	pterm.Println()
@@ -14,77 +88,14 @@ func HandleExistingDatabase(databaseBackupsDirPath, databaseFilePath, backupFile
 	if files.FileExists(databaseFilePath) {
 		spinner.Info("Database found...")
 
-		ThemeDefault := pterm.ThemeDefault
+		var howToHandleExistingDatabase string
+		howToHandleExistingDatabase = selectDatabaseActionOption(databaseBackupsDirPath, databaseFilePath, backupFileNameBase, relayName)
 
-		// Supported database action options
-		options := []string{BackupDatabaseFileOption, UseExistingDatabaseFileOption, OverwriteDatabaseFileOption}
-
-		databaseActionSelector := pterm.InteractiveSelectPrinter{
-			TextStyle:     &ThemeDefault.PrimaryStyle,
-			DefaultText:   "Please select an option",
-			Options:       []string{},
-			OptionStyle:   &ThemeDefault.DefaultText,
-			DefaultOption: "",
-			MaxHeight:     3,
-			Selector:      ">",
-			SelectorStyle: &ThemeDefault.SecondaryStyle,
-			Filter:        true,
+		for howToHandleExistingDatabase == "no" {
+			howToHandleExistingDatabase = selectDatabaseActionOption(databaseBackupsDirPath, databaseFilePath, backupFileNameBase, relayName)
 		}
 
-		pterm.Println()
-		selectedDatabaseActionOption, _ := databaseActionSelector.WithOptions(options).Show()
-
-		// Display the selected option to the user with a green color for emphasis
-		pterm.Info.Printfln("Selected option: %s", pterm.Green(selectedDatabaseActionOption))
-
-		pterm.Println()
-		if selectedDatabaseActionOption == BackupDatabaseFileOption {
-			pterm.Println(pterm.LightCyan("Creating database backup..."))
-			pterm.Println()
-			BackupDatabase(databaseBackupsDirPath, databaseFilePath, backupFileNameBase, relayName)
-			return BackupDatabaseFileOption
-		} else if selectedDatabaseActionOption == UseExistingDatabaseFileOption {
-			pterm.Println(pterm.LightCyan("Using existing database..."))
-			pterm.Println()
-			return UseExistingDatabaseFileOption
-		} else if selectedDatabaseActionOption == OverwriteDatabaseFileOption {
-			prompt := pterm.InteractiveContinuePrinter{
-				DefaultValueIndex: 0,
-				DefaultText:       "Overwrite database?",
-				TextStyle:         &ThemeDefault.PrimaryStyle,
-				Options:           []string{"no", "yes"},
-				OptionsStyle:      &ThemeDefault.SuccessMessageStyle,
-				SuffixStyle:       &ThemeDefault.SecondaryStyle,
-				Delimiter:         ": ",
-			}
-
-			pterm.Println(pterm.Yellow("Warning: Are you sure you want to overwrite your existing database?"))
-			pterm.Printfln(pterm.Yellow("If you select 'yes', then the following database will be overwritten: %s"), databaseFilePath)
-			pterm.Println()
-
-			result, _ := prompt.Show()
-
-			if result == "no" {
-				// TODO
-				// Display options again
-				pterm.Println()
-				pterm.Println(pterm.LightCyan("Exiting wizard..."))
-				os.Exit(1)
-			} else if result == "yes" {
-				pterm.Println()
-				pterm.Println(pterm.LightCyan("Database will be overwitten..."))
-				pterm.Println()
-				return OverwriteDatabaseFileOption
-			} else {
-				pterm.Println()
-				pterm.Error.Println(("Failed to confirm database overwrite action"))
-				os.Exit(1)
-			}
-		} else {
-			pterm.Println()
-			pterm.Error.Println(("Failed to perform selected database action"))
-			os.Exit(1)
-		}
+		return howToHandleExistingDatabase
 	} else {
 		spinner.Info("Database not found continuing with installation...")
 		pterm.Println()
